8aceb1371706f981dde12b925484815683cbba40
Seamus Lee <seamuslee001@gmail.com>
2020-08-20 19:33:30 +1000

    Fix Regression from 5.28.0 which causes Activity Search to break

--- activitytypeacl.php
+++ activitytypeacl.php
@@ -431,7 +431,13 @@ function activitytypeacl_civicrm_selectWhereClause($entity, &$clauses) {
   if ($entity == "Activity") {
     $constituent = CRM_Core_Session::singleton()->get('isConstituent');
     if (!$constituent) {
-      $clauses['activity_type_id'] = array(CRM_ActivityTypeACL_BAO_ACL::getAdditionalActivityClause($where, "report"));
+      $whereClause = CRM_ActivityTypeACL_BAO_ACL::getAdditionalActivityClause($where, "report");
+      if (!empty($clauses['activity_type_id'])) {
+        $clauses['activity_type_id'] .= $whereClause;
+      }
+      else {
+        $clauses['activity_type_id'] = $whereClause;
+      }
     }
   }
 }
